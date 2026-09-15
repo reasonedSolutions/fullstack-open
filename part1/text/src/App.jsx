6b372ff1644fcbc9b2ddd877babefd1cb1f894f8
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-15 22:33:19 +0200

    reached end of part 1a

--- part1/text/src/App.jsx
+++ part1/text/src/App.jsx
@@ -1,8 +1,24 @@
-const App = () => {
-  return (
+const Hello = (props) => {
+  console.log(props)
+  return(
     <div>
-      <p>Hello World</p>
+      <p>Hello {props.name}, you are {props.trait}</p>
     </div>
+  )
+}
+
+const App = () => {
+  console.log("hello from component")
+
+  const friends = [ 'Tan', 'Yuxin' ]
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name='Ka Ming' trait='anxious'/>
+      <Hello name= 'Leo' trait='industrious'/>
+      <Hello name='Andy' trait='chill'/>
+      <p>{friends}</p>
+    </>
   )
 }
 
