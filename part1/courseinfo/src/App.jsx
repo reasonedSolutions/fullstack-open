878e243af66c1cc88f5ee6df585a6d5d6b3fbc36
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-15 22:52:18 +0200

    [1.1] finished

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -12,6 +12,12 @@ const Content = (props) => {
   )
 }
 
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -27,7 +33,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1}/>
       <Content part={part2} exercises={exercises2}/>
       <Content part={part3} exercises={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
 }
