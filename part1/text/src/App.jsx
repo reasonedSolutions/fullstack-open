65168879c0d08299148ead92548cd7942be8b9c0
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-17 21:55:55 +0200

    [1c] end event handling

--- part1/text/src/App.jsx
+++ part1/text/src/App.jsx
@@ -1,7 +1,20 @@
-const App = ({counter}) => {
-  return (
-    <div>{counter}</div>
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return(
+    <div>
+      {count}
+      <button onClick={() => setCount(count+1)}>
+        plus
+      </button>
+      <button onClick={() => setCount(0)}>
+        reset
+      </button>
+    </div>
   )
+
 }
 
 export default App
