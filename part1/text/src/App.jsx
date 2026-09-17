45fd2e72c33ff167ca547047faa1a1cf89de88e0
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-17 22:05:19 +0200

    [1c] end of passing state[...]

--- part1/text/src/App.jsx
+++ part1/text/src/App.jsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
+const Display = (props) => {
+  return (
+    <div>{props.count}</div>
+  )
+}
+
+const Button = (props) => {
+  return(
+    <button onClick={props.handler}>
+      {props.text}
+    </button> 
+  )
+}
 
 const App = () => {
   const [count, setCount] = useState(0)
 
+  const addOne = () => {
+    setCount(count + 1)
+  }
+
+  const resetZero = () => {
+    setCount(0)
+  }
+
+  const subtractOne = () => {
+    setCount(count - 1)
+  }
+
   return(
     <div>
-      {count}
-      <button onClick={() => setCount(count+1)}>
-        plus
-      </button>
-      <button onClick={() => setCount(0)}>
-        reset
-      </button>
+      <Display count={count}/>
+      <Button handler={addOne} text='plus'/>
+      <Button handler={subtractOne} text='minus'/>
+      <Button handler={resetZero} text='reset'/>
     </div>
   )
 
