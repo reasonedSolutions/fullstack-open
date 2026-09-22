1b6e7a9323e2579c23bc6b9c553e7d23c9ac0ff5
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-23 00:01:48 +0200

    [1.7] finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,39 +1,57 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return (
+    <h2>{props.text}</h2>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
+const StatLine = (props) => {
+  if (isNaN(props.number)) {
+    return (
+      <div>{props.type}</div>
+    )
+  }
+  return(
+    <div>{props.type} {props.number} {props.sign}</div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
 
-  const Header = (props) => {
-    return (
-      <h2>{props.text}</h2>
-    )
-  }
-  
-  const Button = (props) => {
-    return (
-      <button onClick={props.onClick}>{props.text}</button>
-    )
-  }
-
-  const StatLine = (props) => {
+  const incrementValue = (variable, setter) => {
+    setTotal(total + 1)
     return(
-      <div>{props.type} {props.number}</div>
+      setter(variable + 1)
     )
   }
 
   return (
     <>
       <Header text='give feedback'/>
-      <Button onClick={() => {setGood(good + 1)}} text='good'/>
-      <Button onClick={() => {setNeutral(neutral + 1)}} text='neutral'/>
-      <Button onClick={() => {setBad(bad + 1)}} text='bad'/>
+      <Button onClick={() => {incrementValue(good, setGood)}} text='good'/>
+      <Button onClick={() => {incrementValue(neutral, setNeutral)}} text='neutral'/>
+      <Button onClick={() => {incrementValue(bad, setBad)}} text='bad'/>
+      {/* <Button onClick={() => {setNeutral(neutral + 1)}} text='neutral'/> */}
+      {/* <Button onClick={() => {setBad(bad + 1)}} text='bad'/> */}
       <Header text='statistics'/>
       <StatLine type='good' number={good}/>
       <StatLine type='neutral' number={neutral}/>
       <StatLine type='bad' number={bad}/>
+      <StatLine type='all' number={total}/>
+      <StatLine type='average' number={(good-bad)/(total)}/>
+      <StatLine type='percentage' number={(good/total)*100} sign='%'/>
     </>
   )
 }
