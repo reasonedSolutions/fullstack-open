1b7af26c5605e1cdbdc3b1750547ccfd4331f078
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-22 21:07:38 +0200

    [1d] end conditional rendering

--- part1/text/src/App.jsx
+++ part1/text/src/App.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
 
-  const [allClicks, setAll] = useState(['record of clicks:'])
+  const [allClicks, setAll] = useState([])
 
   const [total, setTotal] = useState(0)
 
@@ -26,11 +43,11 @@ const App = () => {
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>right</button>
+      <Button onClick={handleLeftClick} text={'left'}/>
+      <Button onClick={handleRightClick} text={'right'}/>
       {right}
       <p>Total clicks: {total}</p>
-      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks}/>
     </div>
   )
 }
