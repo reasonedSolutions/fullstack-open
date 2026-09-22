4d6889d98682758ca2bb11efd33379c829d1f86f
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-22 23:35:56 +0200

    [1.6] finished

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,40 @@
 import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const Header = (props) => {
+    return (
+      <h2>{props.text}</h2>
+    )
+  }
+  
+  const Button = (props) => {
+    return (
+      <button onClick={props.onClick}>{props.text}</button>
+    )
+  }
+
+  const StatLine = (props) => {
+    return(
+      <div>{props.type} {props.number}</div>
+    )
+  }
 
   return (
-    <div></div>
+    <>
+      <Header text='give feedback'/>
+      <Button onClick={() => {setGood(good + 1)}} text='good'/>
+      <Button onClick={() => {setNeutral(neutral + 1)}} text='neutral'/>
+      <Button onClick={() => {setBad(bad + 1)}} text='bad'/>
+      <Header text='statistics'/>
+      <StatLine type='good' number={good}/>
+      <StatLine type='neutral' number={neutral}/>
+      <StatLine type='bad' number={bad}/>
+    </>
   )
 }
 
