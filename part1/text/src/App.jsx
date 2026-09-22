7cc2a45bf47936b7e83c0b018b166f8afbbabf9c
Ka Ming Wong <ka.ming.wong.154@gmail.com>
2026-09-22 21:03:31 +0200

    [1d] asynch update demo

--- part1/text/src/App.jsx
+++ part1/text/src/App.jsx
@@ -4,16 +4,33 @@ const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
 
+  const [allClicks, setAll] = useState(['record of clicks:'])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft+right)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(updatedRight + left)
+  }
+
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
       {right}
+      <p>Total clicks: {total}</p>
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
